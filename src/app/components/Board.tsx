a75b33fc36af444a470b20676ab1d932ca2ad342
marwinburesch <marwin@posteo.de>
2021-08-18 14:24:55 +0200

    ✨ Add win condition

--- src/app/components/Board.tsx
+++ src/app/components/Board.tsx
@@ -10,13 +10,22 @@ function Board(): JSX.Element {
   }
 
   function handleClick(i: number) {
+    if (calculateWinner(squares) || squares[i]) {
+      return;
+    }
     const newSquares = [...squares];
     newSquares[i] = xIsNext ? 'X' : 'O';
     setSquares(newSquares);
     setXIsNext(!xIsNext);
   }
 
-  const status = 'Next player: X';
+  const winner = calculateWinner(squares);
+  let status: string;
+  if (winner) {
+    status = 'Winner: ' + winner;
+  } else {
+    status = 'Next player: ' + (xIsNext ? 'X' : 'O');
+  }
 
   return (
     <div>
@@ -38,6 +47,26 @@ function Board(): JSX.Element {
       </div>
     </div>
   );
+}
+
+function calculateWinner(squares: string[]): string | null {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
 }
 
 export default Board;
